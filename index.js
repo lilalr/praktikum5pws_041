de6d7c001f0eb1723aff06029313513f2f0322dd
lilalr <gurennut@gmail.com>
2025-11-10 12:23:18 +0700

    membuat endpoint check utk check apikey

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ app.post("/generate-api-key", (req, res) => {
   res.json({ apiKey: key });
 });
 
+app.post("/check", (req, res) => {
+  const { apikey } = req.body;
+
+  console.log("Request diterima di /check. API Key:", apikey);
+
+  if (apikey && apikey.startsWith("sk_live_")) {
+    res.json({
+      status: "sukses",
+      message: "API key valid!",
+    });
+  } else {
+
+    
